87adf816a2e83fb406d9346c5a32bd76e08943a1
ksuomala <ksuomala@student.hive.fi>
2020-11-25 17:18:20 +0200

    new test playa

---
+++ main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksuomala <ksuomala@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/25 14:49:30 by ksuomala          #+#    #+#             */
+/*   Updated: 2020/11/25 17:15:27 by ksuomala         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+// typedef struct  s_grid
+// {
+//     char    **board;
+//     char    **piece;
+//     char    player;
+//     int     size;
+// }               t_board;
+
+// ft_get_map(char **board)
+// {
+//     t_board
+// }
+
+char    *filler(char **s)
+{
+    char *ss = "8 1";
+    printf("%s", ss);
+    return (ss);
+}
+int     main(int argc, char **argv)
+{
+    filler(argv);
+}
