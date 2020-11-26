b7db74092ed235d0a931a3ba65e0d5408f5e5fc1
ksuomala <ksuomala@student.hive.fi>
2020-11-26 15:15:25 +0200

    added functions

--- filler.h
+++ filler.h
@@ -1,9 +1,24 @@
 #include "libft/libft.h"
 
+typedef struct s_coordinate
+{
+    int y;
+    int x;
+}              t_coordinate;
+
+
+typedef struct  s_token
+{
+    t_coordinate    *cr;
+    int             len;
+    int             w;
+    int             h;
+}               t_token;
+
 typedef struct  s_grid
 {
+    t_token *piece;
     char    **board;
-    char    **piece;
     int     **map;
     char    p;
     int     size;
@@ -11,5 +26,7 @@ typedef struct  s_grid
     int     columns;
 }               t_board;
 
-t_board    ft_get_board(void);
-int    **ft_minesweeper(t_board f);
+t_board     ft_get_board(void);
+t_token     *ft_get_piece(void);
+char        ft_get_player(void);
+int         **ft_minesweeper(t_board f);
