8c0d6e54babc74080623042509f412a832288ae4
ksuomala <ksuomala@student.hive.fi>
2020-12-09 04:14:07 +0200

    Works quite well and plays most games till the end

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ksuomala <ksuomala@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 14:49:30 by ksuomala          #+#    #+#             */
-/*   Updated: 2020/12/08 20:30:24 by ksuomala         ###   ########.fr       */
+/*   Updated: 2020/12/09 03:42:46 by ksuomala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,6 @@ int     ft_get_data(t_board *filler)
  //   ft_dprintf(fd, "playa is %c", filler.p);
  // HERE is the segfault!
     filler->map = ft_minesweeper(*filler);
-    ft_putchar('b');
     filler->piece = ft_get_piece();
   //  filler_test(fd, *filler);
     return (1);
@@ -71,7 +70,6 @@ int     main(void)
     while (1)
     {
         ft_get_data(&filler);
-        ft_putchar('a');
         move = ft_next_move(filler);
         out = ft_parse_coord(move);
  //       ft_dprintf(fd, "%s\n", out);
