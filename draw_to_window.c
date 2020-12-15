52409ef5114bbbab81d455301d1ba9b262523aa8
ksuomala <ksuomala@student.hive.fi>
2020-12-15 18:13:50 +0200

    made it prettier

--- draw_to_window.c
+++ draw_to_window.c
@@ -6,7 +6,7 @@
 /*   By: ksuomala <ksuomala@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/12 20:11:16 by ksuomala          #+#    #+#             */
-/*   Updated: 2020/12/15 17:20:20 by ksuomala         ###   ########.fr       */
+/*   Updated: 2020/12/15 18:13:24 by ksuomala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,34 +59,36 @@ int game_to_window(t_filler filler)
 
 void        players_to_window(SDL_Renderer *renderer)
 {
+    //TTF_Init();
 
-TTF_Font* Sans = TTF_OpenFont("Sans.ttf", 24); //this opens a font style and sets a size
+    TTF_Font* Sans = TTF_OpenFont("Sans.ttf", 24); //this opens a font style and sets a size
 
-SDL_Color White = {255, 255, 255};  // this is the color in rgb format, maxing out all would give you the color white, and it will be your text's color
+    SDL_Color White = {255, 255, 255};  // this is the color in rgb format, maxing out all would give you the color white, and it will be your text's color
 
-SDL_Surface* surfaceMessage = TTF_RenderText_Solid(Sans, "put your text here", White); // as TTF_RenderText_Solid could only be used on SDL_Surface then you have to create the surface first
+    SDL_Surface* surfaceMessage = TTF_RenderText_Solid(Sans, "put your text here", White); // as TTF_RenderText_Solid could only be used on SDL_Surface then you have to create the surface first
+    if (!surfaceMessage)
+        printf("Unable to get surfaceMessage!");
+    ft_printf("This is just for test purposes\n");
+    SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage); //now you can convert it into a texture
 
-ft_printf("This is just for test purposes\n");
-SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage); //now you can convert it into a texture
-
-SDL_Rect Message_rect; //create a rect
-Message_rect.x = 100;  //controls the rect's x coordinate 
-Message_rect.y = 100; // controls the rect's y coordinte
-Message_rect.w = 100; // controls the width of the rect
-Message_rect.h = 100; // controls the height of the rect
+    SDL_Rect Message_rect; //create a rect
+    Message_rect.x = 100;  //controls the rect's x coordinate 
+    Message_rect.y = 100; // controls the rect's y coordinte
+    Message_rect.w = 100; // controls the width of the rect
+    Message_rect.h = 100; // controls the height of the rect
 
 
-//Mind you that (0,0) is on the top left of the window/screen, think a rect as the text's box, that way it would be very simple to understand
+    //Mind you that (0,0) is on the top left of the window/screen, think a rect as the text's box, that way it would be very simple to understand
 
-//Now since it's a texture, you have to put RenderCopy in your game loop area, the area where the whole code executes
+    //Now since it's a texture, you have to put RenderCopy in your game loop area, the area where the whole code executes
 
-SDL_RenderCopy(renderer, Message, NULL, &Message_rect); //you put the renderer's name first, the Message, the crop size(you can ignore this if you don't want to dabble with cropping), and the rect which is the size and coordinate of your texture
+    SDL_RenderCopy(renderer, Message, NULL, &Message_rect); //you put the renderer's name first, the Message, the crop size(you can ignore this if you don't want to dabble with cropping), and the rect which is the size and coordinate of your texture
 
-SDL_RenderPresent(renderer);
-SDL_Delay(4000);
-//Don't forget to free your surface and texture
-SDL_FreeSurface(surfaceMessage);
-SDL_DestroyTexture(Message);
+    //SDL_RenderPresent(renderer);
+    //SDL_Delay(4000);
+    //Don't forget to free your surface and texture
+    SDL_FreeSurface(surfaceMessage);
+    SDL_DestroyTexture(Message);
 }
 
 SDL_Renderer *background(t_filler *data)
