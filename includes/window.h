72c9514502c20d90aab9460ab5a263cb8f2d2ba6
ksuomala <ksuomala@student.hive.fi>
2020-12-16 04:59:56 +0200

    new function prototypes

--- includes/window.h
+++ includes/window.h
@@ -6,28 +6,30 @@
 #include <fcntl.h>
 #include "filler.h"
 
-typedef struct s_filler 
+typedef struct		s_filler
 {
-    SDL_Window      *win;
-    SDL_Renderer    *renderer;
-    char            **board;
-    int             h;
-    int             w;
-    int             square_size;
-    char            *p1;
-    char            *p2;
-    char            *score_1;
-    char            *score_2;
-}               t_filler;
+	SDL_Window		*win;
+	SDL_Renderer	*renderer;
+	char			**board;
+	int				h;
+	int				w;
+	int				square_size;
+	char			*p1;
+	char			*p2;
+	char			*score_1;
+	char			*score_2;
+}					t_filler;
 
 
-void            ft_error(int msg);
-char            **get_board(size_t height, size_t width);
-t_filler        get_data(SDL_Window *w);
-char            **cpy_board(char **board, size_t n);
+void			ft_error(int msg);
+char			**get_board(size_t height, size_t width);
+t_filler		get_data(SDL_Window *w);
+char			**cpy_board(char **board, size_t n);
+void			show_score(t_filler *data);
 
-void            square_to_window(t_filler filler, int y, int x);
-int             game_to_window(t_filler filler);
-SDL_Renderer    *background(t_filler *data);
+void			square_to_window(t_filler filler, int y, int x);
+int				game_to_window(t_filler filler);
+SDL_Renderer	*background(t_filler *data);
+void			text_to_window(SDL_Renderer *renderer, SDL_Color color, char *message, int location[1]);
 
 #endif
