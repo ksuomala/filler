68a2d9519b375ff58b528ceace423d29ccbbf024
ksuomala <ksuomala@student.hive.fi>
2020-12-17 01:34:24 +0200

    runs vm with visualizer

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./resources/filler_vm_old -f resources/maps/map00 -p1 ./ksuomala.filler -p2 ./ksuomala.filler
+./resources/filler_vm_old -f resources/maps/map02 -p1 ./ksuomala.filler -p2 ./ksuomala.filler
